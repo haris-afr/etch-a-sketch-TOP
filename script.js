ce0bf1d6728cdefc3b31a3aba58c0b3f85d35ffb
haris-afr <haris.afridi1312@gmail.com>
2025-06-30 15:55:57 +0500

    make single line grid

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("Hello World!")
+const gridContainer = document.querySelector(".grid-container");
+
+for (i = 0; i < 16; i++){
+    const gridBox = document.createElement("div");
+    gridBox.classList.add("gridbox")
+    gridBox.setAttribute("style", "border: 5px solid black; width: 200px; height: 200px");
+    gridContainer.appendChild(gridBox);
+}
