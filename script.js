9307cc6b214b043cc3796d5e01240424593a79fc
haris-afr <haris.afridi1312@gmail.com>
2025-06-30 17:47:18 +0500

    make dynamic grid creation functionality

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const gridContainer = document.querySelector(".grid-container");
+const sliderValueDisplay = document.querySelector(".size-select-container p");
+const slider = document.querySelector(".slider");
+
 
 function toggleColor(element){
     const bgColor = element.target.style.backgroundColor;
-    console.log(bgColor);
+    
     if (bgColor == "rgb(241, 239, 236)"){
         element.target.style.backgroundColor = "rgb(212, 201, 190)";
     }
@@ -11,14 +14,33 @@ function toggleColor(element){
     }
 }
 
-// creates, styles and appends grid boxes to the container element
-for (i = 0; i < 16; i++){
-    const gridBox = document.createElement("div");
-    gridBox.classList.add("grid-box");
-    gridBox.setAttribute("style", "border: 5px solid rgb(3, 3, 3); width: 200px; height: 200px; background-color: rgb(241, 239, 236)");
+function createGrid(gridLen){
+    for (i = 0; i < gridLen * gridLen; i++){
+        const gridBox = document.createElement("div");
+        gridBox.classList.add("grid-box");
+        gridBox.setAttribute("style", "border: 5px solid rgb(3, 3, 3); width: 200px; height: 200px; background-color: rgb(241, 239, 236)");
 
-    gridBox.addEventListener("mouseenter", (element) => toggleColor(element));
+        gridBox.addEventListener("mouseenter", (element) => toggleColor(element));
 
-    gridContainer.appendChild(gridBox);
+        gridContainer.appendChild(gridBox);
+    }
 }
 
+function destroyGrid(){
+    while(gridContainer.firstChild){
+        gridContainer.removeChild(gridContainer.lastChild);
+    }
+}
+
+// creates, styles and appends grid boxes to the container element
+for (i = 0; i < 16; i++){
+
+}
+
+slider.addEventListener("input", (evt) => {
+    const sliderVal = evt.target.value;
+    sliderValueDisplay.textContent = sliderVal;
+
+    destroyGrid();
+    createGrid(sliderVal);
+})
