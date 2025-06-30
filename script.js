0ea4be73597e434f20bc54ac97c9cbf742441bb8
haris-afr <haris.afridi1312@gmail.com>
2025-06-30 22:13:04 +0500

    add randomize color functionality

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const gridContainer = document.querySelector(".grid-container");
 const sliderValueDisplay = document.querySelector(".size-select-container p");
 const slider = document.querySelector(".slider");
+const randomRGBButton = document.querySelector(".random-rgb-button")
 
+let randomRGB = 0;
 
 function toggleColor(element){
     const bgColor = element.target.style.backgroundColor;
@@ -14,16 +16,26 @@ function toggleColor(element){
     }
 }
 
+function randomizeColor(element){
+    const val1 = Math.floor(Math.random() * 256);
+    const val2 = Math.floor(Math.random() * 256);
+    const val3 = Math.floor(Math.random() * 256);
+    element.target.style.backgroundColor = "rgb(" + val1 + ", " + val2 + ", " + val3 + ")"; 
+}
+
 function createGrid(gridLen){
     for (i = 0; i < gridLen * gridLen; i++){
         const gridBox = document.createElement("div");
         gridBox.classList.add("grid-box");
 
         gridBox.setAttribute("style", "border: 1px solid rgb(3, 3, 3); background-color: rgb(241, 239, 236)");
-        gridBox.style.width = 800 / gridLen + "px";
-        gridBox.style.height = 800 / gridLen + "px";
+        gridBox.style.width = (800 / gridLen) + "px";
+        gridBox.style.height = (800 / gridLen) + "px";
         
-        gridBox.addEventListener("mouseenter", (element) => toggleColor(element));
+
+        gridBox.addEventListener("mouseenter", (element) => {
+            randomRGB ? randomizeColor(element) : toggleColor(element);
+        });
 
         gridContainer.appendChild(gridBox);
     }
@@ -42,6 +54,10 @@ slider.addEventListener("input", (evt) => {
 
     destroyGrid();
     createGrid(sliderVal);
+})
+
+randomRGBButton.addEventListener("input", () => {
+    randomRGB = !(randomRGB);
 })
 
 createGrid(4);
