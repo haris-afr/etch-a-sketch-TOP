9ceb348f844cba264a459d657ec00da8aae52f0d
haris-afr <haris.afridi1312@gmail.com>
2025-06-30 16:27:40 +0500

    add color change functionality

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 const gridContainer = document.querySelector(".grid-container");
 
+function toggleColor(element){
+    const bgColor = element.target.style.backgroundColor;
+    console.log(bgColor);
+    if (bgColor == "rgb(241, 239, 236)"){
+        element.target.style.backgroundColor = "rgb(212, 201, 190)";
+    }
+    else if (bgColor == "rgb(212, 201, 190)"){
+        element.target.style.backgroundColor = "rgb(241, 239, 236)";
+    }
+}
+
+// creates, styles and appends grid boxes to the container element
 for (i = 0; i < 16; i++){
     const gridBox = document.createElement("div");
-    gridBox.classList.add("gridbox")
-    gridBox.setAttribute("style", "border: 5px solid black; width: 200px; height: 200px");
+    gridBox.classList.add("grid-box");
+    gridBox.setAttribute("style", "border: 5px solid rgb(3, 3, 3); width: 200px; height: 200px; background-color: rgb(241, 239, 236)");
+
+    gridBox.addEventListener("mouseenter", (element) => toggleColor(element));
+
     gridContainer.appendChild(gridBox);
 }
+
